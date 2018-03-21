ab14b081bdfd353038cefcb92c493e6d2d999663
Jakub Bondyra <jb10193@gmail.com>
2018-03-21 21:02:25 +0100

    initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='pyBreakDown',
+      version='0.0.1',
+      description='breakDown python implementation',
+      url='http://github.com/bondyra/pyBreakDown',
+      author='Jakub Bondyra',
+      author_email='jb10193@gmail.com',
+      license='GPL-2',
+      packages= find_packages(exclude=['tests']))
+
