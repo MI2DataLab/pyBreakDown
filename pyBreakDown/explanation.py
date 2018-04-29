1143d2d7f9e8a3ef50d42ba9f0dfd52471bfaf14
Jakub Bondyra <jb10193@gmail.com>
2018-04-30 01:06:26 +0200

    visualization functionalities

--- pyBreakDown/explanation.py
+++ pyBreakDown/explanation.py
@@ -1,5 +1,10 @@
 import numpy as np
 from collections import deque
+from recordclass import recordclass
+import matplotlib.patches as patches
+from matplotlib import pyplot as plt
+
+AttrInfo = recordclass("AttrInfo",["name","value","contribution","cumulative"])
 
 class Explanation:
     """
@@ -9,9 +14,15 @@ class Explanation:
     _INTERCEPT_VALUE = 1
 
     def __init__ (self, variable_names, variable_values, contributions):
-        self._variable_names = deque(variable_names)
-        self._variable_values = deque(variable_values)
-        self._contributions = deque(contributions)
+        self._attributes = deque()
+        csum = 0
+        for (name, value, contribution) in zip(variable_names, variable_values, contributions):
+            csum+=contribution
+            self._attributes.append(
+                AttrInfo(name=name, value=value, contribution=contribution, cumulative=csum)
+                )
+        self._has_intercept=False
+        self._has_final_prognosis=False
     
     def text (self, fwidth=25, contwidth=20, cumulwidth = 20, digits=2):
         """
@@ -27,25 +38,17 @@ class Explanation:
             Width of column with cumulative values, in digits.
         digits : int
             Number of decimal places for values.
-
         """
+        if not self._has_intercept or not self._has_final_prognosis:
+            return
 
-        assert len(self._variable_names) == len(self._variable_values)
-        assert len(self._variable_names) == len(self._contributions)
-        assert len(self._variable_names) == len(self._cumulative)
-        feats = [
-            ' = '.join([name, str(val)]) 
-            for name, val 
-                in zip (self._variable_names, self._variable_values)
+        lines = [''.join(
+                [
+                ' = '.join([attr.name, str(attr.value)]).ljust(fwidth), 
+                str(round(attr.contribution,digits)).ljust(contwidth), 
+                str(round(attr.cumulative, digits)).ljust(cumulwidth)
                 ]
-        lines = [
-            ''.join(
-            [feats[i].ljust(fwidth), 
-            str(round(self._contributions[i],digits)).ljust(contwidth), 
-            str(round(self._cumulative[i], digits)).ljust(cumulwidth)]
-            ) 
-            for i in range(0,len(self._contributions))
-                ]
+                ) for attr in self._attributes]
 
         print (''.join(
             ["Feature".ljust(fwidth), 
@@ -57,17 +60,92 @@ class Explanation:
             str(round(self._final_prediction, digits)).ljust(cumulwidth)]))
         print(' = '.join(["Baseline", str(round(self._baseline, digits))]))
 
-    def visualize(self):
-        pass
+    def visualize(self, figsize=(14,12), filename=None, dpi=90):
+        """
+        Get user friendly visualization of explanation
+
+        Parameters
+        ----------
+        figsize : tuple int
+            Pyplot figure size
+        filename : string
+            Name of file to save the visualization. 
+            If not specified, standard pyplot.show() will be performed.
+        dpi : int
+            Digits per inch for saving to the file
+        """
+
+        if not self._has_intercept or not self._has_final_prognosis:
+            return
+
+        fig = plt.figure(figsize=figsize)
+        ax = fig.add_subplot(111,aspect='equal')
+        positions = list(range(len(self._attributes)+1))
+        previous_value = 0
+        for (attr_info, position) in zip(self._attributes, positions):
+            cumulative = attr_info.cumulative+self._baseline
+            height=1
+            left = previous_value if attr_info.contribution > 0 else cumulative
+            width = abs(attr_info.contribution)
+            color = "blue" if attr_info.contribution > 0 else "orange"
+            rect = patches.Rectangle(
+                xy=(left, position-0.5),width=width,height=height,alpha=0.8,color=color)
+            ax.add_patch(rect)
+            plt.errorbar(x=left, y=position, yerr=0.5, color="black")
+            plt.errorbar(x=left+width, y=position, yerr=0.5, color="black")
+            plt.text(left+width+0.1,y=position-0.2,s=str(round(attr_info.contribution,2)),size=16)
+            previous_value = cumulative
+        
+        #add final prediction bar
+        rectf = patches.Rectangle(
+            xy=(self._baseline,positions[len(positions)-1]-0.5), 
+            width=self._final_prediction, 
+            height=1, color="grey", alpha=0.8
+        )
+        ax.add_patch(rectf)
+        ax.axvline(x=self._baseline,mew=3,color="black",alpha=1)
+        plt.errorbar(x=self._baseline, y=len(positions)-1, yerr=0.5, color="black")
+        plt.errorbar(x=self._baseline+self._final_prediction, y=len(positions)-1, yerr=0.5, color="black")
+        plt.text(
+            x=self._baseline+self._final_prediction+0.1,
+            y=positions[len(positions)-1]-0.2,
+            s=str(round(self._final_prediction,2)),size=16)
+
+        ax.set_yticks(positions)
+        ax.grid(color="gray",alpha=0.3)
+        labels=["=".join([attr.name,str(attr.value)]) for attr in self._attributes]+["Final Prognosis"]
+        ax.set_yticklabels(labels,size=16)
+        
+        all_cumulative = [attr.cumulative for attr in self._attributes]
+        leftbound = min([min(all_cumulative), 0]) + self._baseline
+        rightbound= max(all_cumulative) + self._baseline
+        ax.set_xlim(leftbound-1, rightbound+3)
+        ax.set_ylim(-1,len(self._attributes)+1)
+
+        if filename is None:
+            plt.show()
+        else:
+            fig.savefig(filename,dpi=dpi)
 
     def add_intercept (self, intercept_contribution):
-        self._variable_names.appendleft(self._INTERCEPT_NAME)
-        self._variable_values.appendleft(self._INTERCEPT_VALUE)
-        self._contributions.appendleft(intercept_contribution)
+        self._attributes.appendleft(AttrInfo(
+            name=self._INTERCEPT_NAME, 
+            value=self._INTERCEPT_VALUE, 
+            contribution=intercept_contribution, 
+            cumulative=0)
+            )
+        self._correct_cumulatives()
+        self._has_intercept = True
 
     def make_final_prediction (self):
-        self._cumulative = deque(np.cumsum(self._contributions))
-        self._final_prediction = sum(self._contributions)
+        self._final_prediction = sum(attr.contribution for attr in self._attributes)
+        self._has_final_prognosis = True
 
     def add_baseline (self, baseline):
         self._baseline = baseline
+
+    def _correct_cumulatives(self):
+        csum = 0
+        for attribute in self._attributes:
+            csum+=attribute.contribution
+            attribute.cumulative = csum
